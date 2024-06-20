39b7aee03638f2edfa49b99c0215562c68a7c7b7
Rewasvat <rewasvat@gmail.com>
2024-06-19 23:43:49 -0300

    DataCache: refactored internal data attr name and fixed saving issue with safe saving of pickled data.

--- nimbus/data.py
+++ nimbus/data.py
@@ -1,9 +1,48 @@
 import os
+import click
 import pickle
+import shutil
 import keyring
 import platform
+import traceback
 from nimbus.utils.command_utils import Singleton
 from typing import Callable
+
+
+def safe_pickle_save(file_path: str, data):
+    """Saves the given data to the given file-path, using pickle to serialize the data.
+
+    However, this does so in a safe manner. If pickle encounters an error while serializing, the file remains corrupted.
+    So this backs up the file, if it existed, and restores it in the case of an error, while printing the error message
+    for debugging.
+
+    Args:
+        file_path (str): Path to file where data will be serialized.
+        data (any): The object to save. It must be pickable.
+
+    Returns:
+        bool: if the file was successfully saved.
+    """
+    backup_path = None
+    if os.path.isfile(file_path):
+        backup_path = file_path + "_BACKUP"
+        shutil.copyfile(file_path, backup_path)
+
+    success = False
+    try:
+        with open(file_path, 'wb') as file_obj:
+            pickle.dump(data, file_obj)
+            success = True
+    except Exception:
+        click.secho(f"Error while trying to save (pickle) object '{data}' to file '{file_path}':\n{traceback.format_exc()}", fg="red")
+
+    if backup_path:
+        if not success:
+            shutil.copyfile(backup_path, file_path)
+            click.secho("The file was restored to its state before trying to pickle.", fg="blue")
+        if os.path.isfile(backup_path):
+            os.remove(backup_path)
+    return success
 
 
 class DataCache(metaclass=Singleton):
@@ -11,15 +50,18 @@ class DataCache(metaclass=Singleton):
     global data as a kind of cache."""
 
     def __init__(self):
-        self.data: dict[str, any] = None
+        self._cache_data: dict[str, any] = None
+        """Dict of data of the main cache. This is persisted via pickle to the common nimbus-datacache file."""
         self.service_id = "NimbusTool"
         self.shutdown_listeners: list[Callable[[], None]] = []
         self.data_path = ""  # Initialized in 'set_cache_path'
+        self.base_path = ""
         self.set_cache_path(os.path.expanduser("~"))
 
     def set_cache_path(self, path):
         """Sets the folder in which this DataCache instance will save data. This defaults to the user's home folder ('~').
         Files saved have the same name independently of the folder in which they were saved."""
+        self.base_path = path
         self.data_path = os.path.join(path, "nimbus_datacache")
 
     def delete(self):
@@ -29,22 +71,25 @@ class DataCache(metaclass=Singleton):
             os.remove(self.data_path)
 
     def load_data(self):
-        """Loads our internal persisted data cache, if it isn't loaded yet."""
-        if self.data is None:
+        """Loads our internal persisted data cache, if it isn't loaded yet.
+
+        Since data is saved/loaded with pickle, when the cache is loaded, all pickled objects in it will be recreated."""
+        if self._cache_data is None:
             if os.path.exists(self.data_path):
                 with open(self.data_path, 'rb') as file_obj:
-                    self.data = pickle.load(file_obj)
+                    self._cache_data = pickle.load(file_obj)
             else:
-                self.data = {}
-        return self.data
+                self._cache_data = {}
+        return self._cache_data
 
     def get_data(self, key, default=None):
         """Gets a data object from the persisted data cache.
         If the object specified by KEY doesn't exist, DEFAULT is returned instead.
 
-        The first time this is called will load the persisted cache from disk."""
+        The first time this is called will load the persisted cache from disk.
+        Since data is saved/loaded with pickle, when the cache is loaded, all pickled objects in it will be recreated."""
         self.load_data()
-        return self.data.get(key, default)
+        return self._cache_data.get(key, default)
 
     def set_data(self, key, value, persist_data=True):
         """Sets a KEY/VALUE pair to the persisted data cache.
@@ -55,17 +100,16 @@ class DataCache(metaclass=Singleton):
         If VALUE is None, then the key is deleted from the dict."""
         self.load_data()
         if value is None:
-            self.data.pop(key, None)
+            self._cache_data.pop(key, None)
         else:
-            self.data[key] = value
+            self._cache_data[key] = value
         if persist_data:
             self.save_data()
 
     def save_data(self):
         """Saves this data cache to disk."""
-        if self.data is not None:
-            with open(self.data_path, 'wb') as file_obj:
-                pickle.dump(self.data, file_obj)
+        if self._cache_data is not None:
+            safe_pickle_save(self.data_path, self._cache_data)
 
     def set_password(self, key, password):
         """Saves a value ("password") in the system's encrypted keyring service.
@@ -132,6 +176,3 @@ class DataCache(metaclass=Singleton):
         """
         self.shutdown_listeners.append(listener)
         return listener
-
-    # TODO: seria interessante ter uma forma, automatica se possivel, de fazer um backup dos dados salvos.
-    #   - ja teve caso que deu erro na escrita enqto codava coisa, e ai o cache inteiro ficou corrompido.
