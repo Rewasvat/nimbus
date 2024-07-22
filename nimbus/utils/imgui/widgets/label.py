3b0ed96358782f1644b45c955c64d72dd6e771f1
Rewasvat <rewasvat@gmail.com>
2024-07-21 22:13:10 -0300

    Refactored Label Widget (TextMixin): - All code actually related to drawing the text is a new TextObject class - Now supports/uses our fonts. - TextMixin uses an internal TextObject

--- nimbus/utils/imgui/widgets/label.py
+++ nimbus/utils/imgui/widgets/label.py
@@ -4,6 +4,7 @@ from nimbus.utils.imgui.widgets.base import LeafWidget, WidgetColors
 from nimbus.utils.imgui.colors import Colors, Color
 from nimbus.utils.imgui.nodes import input_property
 from nimbus.utils.imgui.math import Vector2, Rectangle
+from nimbus.utils.imgui.fonts import FontDatabase, Fonts
 from imgui_bundle import imgui
 from enum import Enum
 
@@ -21,97 +22,147 @@ class TextAlignment(Enum):
     CENTER = "CENTER"
 
 
-# TODO: implementar margin pra renderizar texto pouco mais longe das bordas qdo não no CENTER
-class TextMixin:
-    """Simple text widget."""
+class TextObject:
+    """Low-level text rendering object in Nimbus's Widgets system.
 
-    def __init__(self, text: str = ""):
-        self.text = text
-        self._wrapped = False
-        self._align: TextAlignment = TextAlignment.CENTER
+    This holds all the necessary data and is capable of rendering text in IMGUI using our standard in the Widgets System.
+    However, this class is completely independent of a widget.
 
-    @input_property(multiline=True)
-    def text(self) -> str:
-        """The text being displayed [GET/SET]"""
-        return ""  # this is essentially the default value.
+    Our purpose is to be used in a widget by association, not inheritance: a widget might hold several TextObjects at the
+    same time, in order to easily render text with different properties.
 
-    @input_property()
-    def text_color(self) -> Color:
-        """The color of the text [GET/SET]"""
-        return Colors.white  # this is essentially the default value.
+    For a specific simple text widget see ``Label``.
+    For a mixin for widget classes that adds simple text rendering (same as Label), see ``TextMixin``.
+    """
 
-    @types.bool_property()
-    def is_wrapped(self):
-        """If the text will wrap around to not exceed our available width space [GET/SET]"""
-        return self._wrapped
+    def __init__(self, text=""):
+        self.text: str = text
+        self.align: TextAlignment = TextAlignment.CENTER
+        self.area = Rectangle()
+        self.wrapped: bool = False
+        self.scale = 1.0
+        self.color = Colors.white
+        self.current_font_size: int = 20
+        self.last_font_size: int = 20
+        self.font_size_counter: int = 0
+        self.font: Fonts = Fonts.LCARS
+        self.debug_draw_boxes = False
+        self._text_rect = Rectangle()
 
-    @is_wrapped.setter
-    def is_wrapped(self, value: bool):
-        self._wrapped = value
+    @property
+    def text_area(self):
+        """The text's bounding rect. This is the actual text position/size inside our area, and as such
+        this rect should be fully contained in it. This is updated everytime ``self.draw()`` is called."""
+        return self._text_rect
 
-    @types.enum_property()
-    def align(self):
-        """How the text is aligned to our area [GET/SET]"""
-        return self._align
+    def draw(self):
+        """Draws this text object to IMGUI.
 
-    @align.setter
-    def align(self, value: TextAlignment):
-        self._align = value
-
-    @input_property(min=0.0, max=2.0, is_slider=True, flags=imgui.SliderFlags_.always_clamp)
-    def scale(self) -> float:
-        """The scale of the text. [GET/SET]
-        * 0 (min): impossible. Since we can't have scale=0, at this value scale will behave as if it was =1.
-        * close to 0: theoretical min size possible, pratically invisible/unreadable.
-        * [0, 1]: scaled down text
-        * 1.0: regular text size
-        * [1, 2]: scaled up text
-        * 2 (max): max size possible in current label slot area.
-
-        Note that scaling won't work properly when text is long, since its regular size might already match our slot size.
+        Text is drawn using IMGUI's DrawLists and our absolute area coords.
+        Text, area, color and other parameters that alter how the text is rendered are all defined as public attributes
+        of this object, so change them at will.
         """
-        return 1.0  # this is essentially the default value.
-
-    def _draw_text(self):
-        """Internal utility to render our label's text."""
         if self.text is not None and len(self.text) > 0:
-            lines = self._format_text(self.text).split("\n")
-            wrap_width = self._area.x if self._wrapped else 0
-            line_rects: list[Rectangle] = []
-            for line in lines:
-                y = line_rects[-1].bottom_left_pos.y if len(line_rects) > 0 else 0
-                line_rect = Rectangle((0, y), imgui.calc_text_size(line, wrap_width=wrap_width))
-                line_rects.append(line_rect)
-            text_rect = sum(line_rects, line_rects[0])
+            lines = self.text.split("\n")
+            wrap_width = self.area.size.x if self.wrapped else 0
 
-            font = imgui.get_font()
-            font_height = font.font_size
-            max_font_scale = (self._area / text_rect.size).min_component()
+            font_size = self.calculate_font_size(lines, wrap_width)
 
+            font_db = FontDatabase()
+            # Second pass to render the text with proper font size.
+            with font_db.using_font(font_size, self.font) as font:
+                self._text_rect, line_rects = self.get_text_rects(lines, wrap_width)
+
+                draw = imgui.get_window_draw_list()
+                if self.debug_draw_boxes:
+                    draw.add_rect(self.text_area.position, self.text_area.bottom_right_pos, Colors.green.u32)
+                for line, line_rect in zip(lines, line_rects):
+                    if self.debug_draw_boxes:
+                        draw.add_rect(line_rect.position, line_rect.bottom_right_pos, Colors.red.u32)
+                    draw.add_text(
+                        font=font,
+                        font_size=font_size,
+                        pos=line_rect.position,
+                        col=self.color.u32,
+                        text_begin=line,
+                        text_end=None,
+                        wrap_width=wrap_width,
+                        cpu_fine_clip_rect=None
+                    )
+
+    def calculate_font_size(self, lines: list[str], wrap_width=0):
+        """Calculates the proper font size to use when rendering the text of this TextMixin.
+
+        This considers this object's area, text, alignment, wrapping and scaling to calculate
+        the font-size required to render the text in the way (and scale) the user wants, without actually scaling
+        the text, which would lead to blurry and otherwise ugly rendered text.
+
+        User selected text-scaling is what most affect the font-size. Since ``self.scale`` can change each frame when editing it,
+        this instance method stores newly calculated values of font-size, but only actually returns an updated value after a few
+        calls to it have calculated the same value.
+
+        Since this is expected to be called once each frame, it'll thus take a few frames to properly update the font
+        size after changing parameters (particularly scale).
+
+        Args:
+            lines (list[str]): list of text lines that would be rendered.
+            wrap_width (int, optional): wrap width to use when calculating text sizes. Defaults to 0.
+
+        Returns:
+            int: font size to render the texts.
+        """
+        font_db = FontDatabase()
+        # First pass with default-sized font to calculate text sizes and proper desired font size
+        with font_db.using_font(20, self.font) as base_font:
+            text_rect = self.get_text_rects(lines, wrap_width)[0]
+
+            max_font_scale = (self.area.size / text_rect.size).min_component()
             actual_scale = 1.0
             if self.scale > 1:
                 actual_scale += (self.scale - 1) * (max_font_scale - 1)
             elif self.scale > 0:
                 actual_scale = self.scale
 
-            text_rect = self._update_line_rects(line_rects, actual_scale)
+            base_font_height = base_font.font_size
+            final_font_size = int(base_font_height * actual_scale)
 
-            draw_list = imgui.get_window_draw_list()
-            # draw_list.add_rect(text_rect.position, text_rect.bottom_right_pos, Colors.green.u32)
-            for line, line_rect in zip(lines, line_rects):
-                # draw_list.add_rect(line_rect.position, line_rect.bottom_right_pos, Colors.red.u32)
-                draw_list.add_text(
-                    font=font,
-                    font_size=font_height*actual_scale,
-                    pos=line_rect.position,
-                    col=self.text_color.u32,
-                    text_begin=line,
-                    text_end=None,
-                    wrap_width=wrap_width,
-                    cpu_fine_clip_rect=None
-                )
+            frames_to_wait = 5
+            if final_font_size != self.last_font_size:
+                self.font_size_counter = 0
+                self.last_font_size = final_font_size
+            elif self.font_size_counter < frames_to_wait:
+                self.font_size_counter += 1
 
-    def _update_line_rects(self, line_rects: list[Rectangle], scale: float):
+            if self.font_size_counter == frames_to_wait and self.current_font_size != self.last_font_size:
+                self.current_font_size = self.last_font_size
+
+        return self.current_font_size
+
+    def get_text_rects(self, lines: list[str], wrap_width=0):
+        """Calculates the text rectangles (position and size) for each of the given text lines, according to our
+        current area and text alignment.
+
+        This just uses the current font. So to get the rects of the proper size for a given font, the font should be changed in imgui
+        before using this.
+
+        Args:
+            lines (list[str]): list of text lines to calculate rects from.
+            wrap_width (int, optional): wrap width to use when calculating text sizes. Defaults to 0.
+
+        Returns:
+            tuple[Rectangle, list[Rectangle]]: a tuple with two values, in order:
+            * ``Rectangle``: the total text rectangle - the position/size rectangle of all lines together.
+            * ``list[Rectangle]``: the individual rectangle for each line (matching indexes to the lines arg).
+        """
+        line_rects: list[Rectangle] = []
+        for line in lines:
+            y = line_rects[-1].bottom_left_pos.y if len(line_rects) > 0 else 0
+            line_rect = Rectangle((0, y), imgui.calc_text_size(line, wrap_width=wrap_width))
+            line_rects.append(line_rect)
+        text_rect = self.update_line_rects(line_rects, 1)
+        return text_rect, line_rects
+
+    def update_line_rects(self, line_rects: list[Rectangle], scale: float):
         """Updates the position and size of each line rectangle according to our current text alignment
         and given scale.
 
@@ -127,23 +178,23 @@ class TextMixin:
         Returns:
             Rectangle: the rectangle bounding all updated line rectangles.
         """
-        pos = Vector2.from_cursor_screen_pos()
+        pos = self.area.position
         prev_y = 0.0
         # Adjust positions of each line.
         for rect in line_rects:
             rect.size = rect.size * scale
-            rect.position = self._get_text_pos(rect.size) + (0, prev_y)
+            rect.position = self.get_text_pos(rect.size) + (0, prev_y)
             prev_y += rect.size.y
 
         # Adjust position of whole group of lines
         text_rect = sum(line_rects, line_rects[0])
         group_offset = text_rect.position
-        text_rect.position = pos + self._get_text_pos(text_rect.size)
+        text_rect.position = pos + self.get_text_pos(text_rect.size)
         for rect in line_rects:
             rect.position += text_rect.position - group_offset
         return text_rect
 
-    def _get_text_pos(self, text_size: Vector2):
+    def get_text_pos(self, text_size: Vector2):
         """Calculates the position offset (top-left corner) of the given text size for drawing in our
         area, given the area size and our text alignment setting.
 
@@ -155,17 +206,95 @@ class TextMixin:
 
         # Update Pos X
         if self.align in (TextAlignment.TOP_RIGHT, TextAlignment.RIGHT, TextAlignment.BOTTOM_RIGHT):
-            pos.x += self._area.x - text_size.x
+            pos.x += self.area.size.x - text_size.x
         elif self.align in (TextAlignment.TOP, TextAlignment.CENTER, TextAlignment.BOTTOM):
-            pos.x += self._area.x * 0.5 - text_size.x * 0.5
+            pos.x += self.area.size.x * 0.5 - text_size.x * 0.5
 
         # Update Pos Y
         if self.align in (TextAlignment.BOTTOM_LEFT, TextAlignment.BOTTOM, TextAlignment.BOTTOM_RIGHT):
-            pos.y += self._area.y - text_size.y - 1
+            pos.y += self.area.size.y - text_size.y - 1
         elif self.align in (TextAlignment.LEFT, TextAlignment.CENTER, TextAlignment.RIGHT):
-            pos.y += self._area.y * 0.5 - text_size.y * 0.5
+            pos.y += self.area.size.y * 0.5 - text_size.y * 0.5
 
         return pos
+
+
+# TODO: implementar margin pra renderizar texto pouco mais longe das bordas qdo não no CENTER
+class TextMixin:
+    """Provides text rendering for a widget using an internal TextObject instance.
+
+    Includes Imgui/NodeData properties for editing the TextObject's attributes.
+    """
+
+    def __init__(self, text: str = ""):
+        self._text_internal = TextObject()
+        self.text = text
+
+    @input_property(multiline=True)
+    def text(self) -> str:
+        """The text being displayed [GET/SET]"""
+        return ""  # this is essentially the default value.
+
+    @input_property()
+    def text_color(self) -> Color:
+        """The color of the text [GET/SET]"""
+        return Colors.white  # this is essentially the default value.
+
+    @types.bool_property()
+    def is_wrapped(self):
+        """If the text will wrap around to not exceed our available width space [GET/SET]"""
+        return self._text_internal.wrapped
+
+    @is_wrapped.setter
+    def is_wrapped(self, value: bool):
+        self._text_internal.wrapped = value
+
+    @types.enum_property()
+    def align(self):
+        """How the text is aligned to our area [GET/SET]"""
+        return self._text_internal.align
+
+    @align.setter
+    def align(self, value: TextAlignment):
+        self._text_internal.align = value
+
+    @input_property(min=0.0, max=2.0, is_slider=True, flags=imgui.SliderFlags_.always_clamp)
+    def scale(self) -> float:
+        """The scale of the text. [GET/SET]
+        * 0 (min): impossible. Since we can't have scale=0, at this value scale will behave as if it was =1.
+        * close to 0: theoretical min size possible, pratically invisible/unreadable.
+        * [0, 1]: scaled down text
+        * 1.0: regular text size (default font-size is 20).
+        * [1, 2]: scaled up text
+        * 2 (max): max size possible in current label slot area.
+
+        Note that scaling won't work properly when text is long, since its regular size might already match our slot size.
+
+        The actual font-size used is calculated based on the Text's properties, with this being the value that most affects it.
+        The calculated font-size is the required size to display the text in the configured way, without actually scaling the
+        text (which would be blurry at best).
+
+        After changing this value, it may take a few moments for the font-size to be properly updated.
+        """
+        return 1.0  # this is essentially the default value.
+
+    @types.enum_property()
+    def font(self) -> Fonts:
+        """Font to use when rendering this text [GET/SET]"""
+        return self._text_internal.font
+
+    @font.setter
+    def font(self, value: Fonts):
+        self._text_internal.font = value
+
+    def _draw_text(self):
+        """Internal utility to render our label's text."""
+        self._text_internal.area.position = self._pos
+        self._text_internal.area.size = self._area
+        self._text_internal.text = self._format_text(self.text)
+        self._text_internal.color = self.text_color
+        self._text_internal.scale = self.scale
+        self._text_internal.draw()
 
     def _format_text(self, text: str):
         """Formats the given text and returns an updated string.
